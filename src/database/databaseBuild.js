2245c10385bab611fa9dbe874040d009a24ac37c
ayah <ayah_kh5@hotmail.com>
2019-10-15 15:15:26 +0300

    add eslint

--- src/database/databaseBuild.js
+++ src/database/databaseBuild.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const client = require("./databaseConnection");
+const client = require('./databaseConnection');
 
 const sql = fs.readFileSync(`${__dirname}/database.sql`).toString();
 
 client.query(sql, (err, result) => {
   if (err) throw err;
-  console.log("Built database with result: ", result);
+  console.log('Built database with result: ', result);
 });
