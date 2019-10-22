96f8b874868dede966da11fcb2919238d022e67b
Mario <mario.saliba98@gmail.com>
2019-10-23 01:31:29 +0300

    adds cors so the api works for localhost requests

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ const path = require('path');
 const express = require('express');
 require('env2')('config.env');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(router);
