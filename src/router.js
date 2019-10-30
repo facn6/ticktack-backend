2eb09920ebfa62ffb3e61f945f8a5b818ccad3b5
DanaEFalah <dana.e.falah@gmail.com>
2019-10-30 15:22:39 +0200

    get request from frontend and send an email

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const artistController = require('./controllers/artist.js');
 const ticketController = require('./controllers/ticket.js');
-
+const emailController = require('./controllers/email.js');
 router
   .route('/artists')
   .get(artistController.getArtist)
@@ -23,6 +23,9 @@ router
   .route('/topartists')
   .get(artistController.gettopArtists)
 
+router
+  .route('/sendEmail')
+  .post(emailController.sendContactEmail)
 // router
 //   .route('/topticketsbydate')
 //   .get(ticketController.getClosestTickets)
